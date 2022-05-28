b10d9a7a3243122ad907c410baf8e87e7813032f
Himanshu Choudhary <raptordev21@gmail.com>
2022-05-28 13:24:28 +0530

    api req and display

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import './App.css'
 
 function App() {
+  const [monsters, setMonsters] = useState([])
+  useEffect(() => {
+    async function getusers() {
+      let res = await fetch('https://jsonplaceholder.typicode.com/users')
+      let users = await res.json()
+      setMonsters(users)
+    }
+    getusers()
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Monsters Rolodex
-        </p>
-      </header>
+      {monsters.map(monster => (
+        <h1 key={monster.id}>{monster.name}</h1>
+      ))}
     </div>
   );
 }
