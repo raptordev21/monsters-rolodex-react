819e17fcabe2025a17ce4d451b7e80053a0c7d1e
Himanshu Choudhary <raptordev21@gmail.com>
2022-06-15 09:37:20 +0530

    search bar added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import CardList from './components/card-list/card-list.component'
 
 function App() {
   const [monsters, setMonsters] = useState([])
+  const [search, setSearch] = useState('')
   useEffect(() => {
     async function getusers() {
       let res = await fetch('https://jsonplaceholder.typicode.com/users')
@@ -12,8 +13,14 @@ function App() {
     }
     getusers()
   }, [])
+
+  const monstersCopy = monsters
+  const searchCopy = search
+
   return (
     <div className="App">
+      <input type="search" name="search" onChange={e => setSearch(e.target.value)} placeholder='Search Monsters' />
+      <div>{search}</div>
       <CardList monsters={monsters} />
     </div>
   );
